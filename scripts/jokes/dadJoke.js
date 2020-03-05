c73e4287f94a627b01e2277f849d02cce89350e1
Shady Grant <shadygrant@Shadys-MacBook-Pro.local>
2020-03-05 15:44:54 -0600

    added some styling

--- scripts/jokes/dadJoke.js
+++ scripts/jokes/dadJoke.js
@@ -1,7 +1,12 @@
 export const dadJokesHtml=(dadJokesObject)=>{
     return `
-    <h4>ID: ${dadJokesObject.id}</h4>
-    <h4>Question: ${dadJokesObject.question}</h4>
-    <h4>Answer: ${dadJokesObject.answer}</h4>
+    <section class="joke">
+            <header class="joke__question">
+                ${dadJokesObject.question}
+            </header>
+            <p class="joke__answer">
+                ${dadJokesObject.answer}
+            </p>
+        </section>
     `
 }
